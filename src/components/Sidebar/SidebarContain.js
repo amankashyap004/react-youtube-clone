39805c05824b50baf03db7526012385dab0983d2
amankashyap004 <amankashyap004@gmail.com>
2022-12-30 13:38:24 +0530

    add sidebar selected option

--- src/components/Sidebar/SidebarContain.js
+++ src/components/Sidebar/SidebarContain.js
@@ -2,8 +2,13 @@ import React from "react";
 import "./Sidebar.css";
 
 export default function SidebarContain(props) {
+   const [selected, setSelected] = React.useState(false);
+   const selectFunc = () => {
+      setSelected(!selected);
+   };
+
    return (
-      <div className="sidebar-contain">
+      <div className={selected?"sidebar-contain sidebar-contain-selected":"sidebar-contain"} onClick={selectFunc}>
          <span className="material-symbols-outlined icon">{props.iconName}</span>
          <p className="sidebar-text">{props.titleName}</p>
       </div>
