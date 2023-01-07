50127acd3520dcb5b99f72c66b33a29fe0a19835
amankashyap004 <amankashyap004@gmail.com>
2023-01-07 18:25:53 +0530

    fix Youtube Video Page folder structure

--- src/components/YoutubeVideoPage/YoutubeVideoPage.js
+++ src/components/YoutubeVideoPage/YoutubeVideoPage.js
@@ -1,17 +1,15 @@
 import React from "react";
 import "./YoutubeVideoPage.css";
-import YoutubeVideoPageVideoContain from "./YoutubeVideoPageVideoContain";
 import YoutubeRecommendationVideo from "../YoutubeRecommendationVideo/YoutubeRecommendationVideo";
-import YoutubeVideoPageSectionVideoDetails from "./YoutubeVideoPageSectionVideoDetails";
+import YoutubeVideoPageVideoDetails from "./YoutubeVideoPageVideoDetails";
 import moment from "moment";
 import { useParams } from "react-router-dom";
 
 export default function YoutubeVideoPage() {
    const youtubeAPIKey = "AIzaSyAvqzRb2G7RmclgTATLtEogCtoec0c2zmE";
    const videoHTTP = "https://www.googleapis.com/youtube/v3/videos?";
-   // const videoId = "HbTON0nb4DU";
-   // const [videoId, setVideoId] = React.useState("");
-   let { videoId } = useParams();
+
+   const { videoId } = useParams();
 
    const fetchUrl = `${videoHTTP}key=${youtubeAPIKey}&id=${videoId}&part=snippet%2CcontentDetails%2Cstatistics`;
    const [apiAllData, setApiAllData] = React.useState(null);
@@ -30,38 +28,35 @@ export default function YoutubeVideoPage() {
    }, []);
 
    return (
-      <section>
-         {/* {apiAllData?.map((item) => {
-            return ( */}
-         <div className="youtube-video-page-section" key={1}>
-            <section className="youtube-video-page-section-video-container">
-               <div className="youtube-video-page-section-video-section">
-                  <YoutubeVideoPageVideoContain videoId={"9fux9swQ5AQ"} />
-               </div>
-               <div className="youtube-video-page-section-video-details">
-                  {apiAllData && (
-                     <YoutubeVideoPageSectionVideoDetails
-                        title={apiAllData?.snippet?.title}
-                        tags={apiAllData?.snippet?.tags}
-                        channelTitle={apiAllData?.snippet?.channelTitle}
-                        description={apiAllData?.snippet?.description}
-                        publishedAt={moment(
-                           `${apiAllData?.snippet?.publishedAt}`,
-                           "YYYYMMDD"
-                        ).fromNow()}
-                        viewCount={apiAllData?.statistics?.viewCount}
-                        likeCount={apiAllData?.statistics?.likeCount}
-                        video={apiAllData}
-                     />
-                  )}
-               </div>
-            </section>
-            <div className="youtube-video-page-section-recommendation">
-               <YoutubeRecommendationVideo />
+      <div className="youtube-video-page-section" key={videoId}>
+         <section className="youtube-video-page-section-video-container">
+            <div className="youtube-video-page-section-video-section">
+               <object
+                  data={`https://www.youtube.com/embed/${videoId}`}
+                  className="youtube-video-page-video-contain-video"
+               ></object>
             </div>
+            <div className="youtube-video-page-section-video-details">
+               {apiAllData && (
+                  <YoutubeVideoPageVideoDetails
+                     title={apiAllData?.snippet?.title}
+                     tags={apiAllData?.snippet?.tags}
+                     channelTitle={apiAllData?.snippet?.channelTitle}
+                     description={apiAllData?.snippet?.description}
+                     publishedAt={moment(
+                        `${apiAllData?.snippet?.publishedAt}`,
+                        "YYYYMMDD"
+                     ).fromNow()}
+                     viewCount={apiAllData?.statistics?.viewCount}
+                     likeCount={apiAllData?.statistics?.likeCount}
+                     video={apiAllData}
+                  />
+               )}
+            </div>
+         </section>
+         <div className="youtube-video-page-section-recommendation">
+            <YoutubeRecommendationVideo />
          </div>
-         {/* );
-         })} */}
-      </section>
+      </div>
    );
 }
