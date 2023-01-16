a356ebbcfb464ac7f1188102275c2ac9954abba6
amankashyap004 <amankashyap004@gmail.com>
2023-01-16 12:12:22 +0530

    fix Youtube Search Filter

--- src/components/YoutubeSearch/YoutubeSearchFilters.js
+++ src/components/YoutubeSearch/YoutubeSearchFilters.js
@@ -1,6 +1,8 @@
 import React from "react";
 
+const uploadDateFilters = ["last hour", "Today", "This week", "This month", "This year"];
 const typeFilters = ["video", "channel", "playlist"];
+const durationFilters = ["Under 4 minutes", "4-20 minutes", "Over 20 minutes"];
 
 export default function YoutubeSearchFilters() {
    const [isFilter, setIsFilter] = React.useState(false);
@@ -24,11 +26,15 @@ export default function YoutubeSearchFilters() {
                   <h5 className="youtube-search-filters-more-heading">UPLOAD DATE</h5>
                   <div className="youtube-search-filters-more-hr"></div>
                   <ul className="youtube-search-filters-more-ul">
-                     <li className="text-para-small">Last hour</li>
-                     <li className="text-para-small">Today</li>
-                     <li className="text-para-small">This week</li>
-                     <li className="text-para-small">This month</li>
-                     <li className="text-para-small">This year</li>
+                     {uploadDateFilters.map((uploadDateFilter, id) => (
+                        <li
+                           key={id}
+                           className="text-para-small"
+                           onClick={() => handleClickValue(uploadDateFilter)}
+                        >
+                           {uploadDateFilter}
+                        </li>
+                     ))}
                   </ul>
                </section>
                <section className="youtube-search-filters-more-container">
@@ -50,9 +56,15 @@ export default function YoutubeSearchFilters() {
                   <h5 className="youtube-search-filters-more-heading">DURATION</h5>
                   <div className="youtube-search-filters-more-hr"></div>
                   <ul className="youtube-search-filters-more-ul">
-                     <li className="text-para-small">Under 4 minutes</li>
-                     <li className="text-para-small">4-20 minutes</li>
-                     <li className="text-para-small">Over 20 minutes</li>
+                     {durationFilters.map((durationFilter, id) => (
+                        <li
+                           key={id}
+                           className="text-para-small"
+                           onClick={() => handleClickValue(durationFilter)}
+                        >
+                           {durationFilter}
+                        </li>
+                     ))}
                   </ul>
                </section>
             </section>
